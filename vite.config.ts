23b7a8fbc87f6e5d9a93c2cabd4d95041dfe5ee7
Hasiel Alvarez <hasielhassan@gmail.com>
2025-11-22 18:12:30 -0800

    Configure deployment and ignore rules

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // For GitHub Pages relative paths
+  base: '/CyberOS/', // Matches the repository name for GitHub Pages
 })
