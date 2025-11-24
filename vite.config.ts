edfc3acc6c43220a3e346ce65f7aef7d4098eb00
Hasiel Alvarez <hasielhassan@gmail.com>
2025-11-23 20:35:03 -0800

    chore: set Vite base path to /CyberOS

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Root base path for custom domain
+  base: '/CyberOS', // Root base path for custom domain
 })
